9a05a16f5167045baac122effd17b22878b0672e
QW <1395213047@qq.com>
2017-12-27 18:33:24 +0800

    change_QW

--- src/Vuecomponent/header.vue
+++ src/Vuecomponent/header.vue
@@ -2,7 +2,7 @@
 	
 	<header>
 		<i class="icon-menu"></i>
-		<img src="../lib/img/123123.jpg" alt="" />
+
 	</header>
 	
 	
@@ -15,11 +15,27 @@
 <style lang="scss">
 	$red: #d33a31;
 	header{
+		position: fixed;
+		top: 0;
 		height: 1.57rem;
 		width: 100%;
 		background: $red;
-		
-		
+		display: flex;
+		align-items: center;
+		i{
+			position: relative;
+			font-size: 0.63rem;
+			color: white;
+		}
+		i:after{
+			position: absolute;
+			display: block;
+			content: '';
+			height: 0.44rem;
+			width: 0.44rem;
+			font-radius: 50%;
+			border: 0.01rem solid blue;
+		}
 	}
 	
 </style>
